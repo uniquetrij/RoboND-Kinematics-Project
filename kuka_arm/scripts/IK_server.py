e76ca1646381cc99419db6aea41b34a86330dfd1
Trijeet <uniquetrij@gmail.com>
2019-01-01 19:28:39 +0530

    writeup update

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -10,6 +10,7 @@ from mpmath import *
 from sympy import *
 
 # Useful utilities
+#
 def to_degrees(rad):
     return rad * 180 / pi
 
@@ -19,30 +20,43 @@ def to_radians(deg):
 
 # Create symbols
 #
-alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
-q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
-d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
-a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7') # twist angles α
+a0,     a1,     a2,     a3,     a4,     a5,     a6     = symbols('a0:7')     # link lengths a
+d1,     d2,     d3,     d4,     d5,     d6,     d7     = symbols('d1:8')     # link offsets d
+q1,     q2,     q3,     q4,     q5,     q6,     q7     = symbols('q1:8')     # joint angles θ
+
 
 # Create Modified DH parameters
 #
 dh_dict = {
-    alpha0: 0, a0: 0, d1: 0.75, q1: q1,
-    alpha1: -pi / 2., a1: 0.35, d2: 0, q2: -pi / 2. + q2,
-    alpha2: 0, a2: 1.25, d3: 0, q3: q3,
-    alpha3: -pi / 2., a3: -0.054, d4: 1.5, q4: q4,
-    alpha4: pi / 2, a4: 0, d5: 0, q5: q5,
-    alpha5: -pi / 2., a5: 0, d6: 0, q6: q6,
-    alpha6: 0, a6: 0, d7: 0.303, q7: 0
+     alpha0:      0, a0:      0, d1:  0.75, q1:         q1,
+     alpha1: -pi/2., a1:   0.35, d2:     0, q2: q2 - pi/2.,
+     alpha2:      0, a2:   1.25, d3:     0, q3:         q3,
+     alpha3: -pi/2., a3: -0.054, d4:  1.50, q4:         q4,
+     alpha4:  pi/2., a4:      0, d5:     0, q5:         q5,
+     alpha5: -pi/2., a5:      0, d6:     0, q6:         q6,
+     alpha6:      0, a6:      0, d7: 0.303, q7:          0
 }
 
 # Define Modified DH Transformation matrix
 #
 def transform(alpha, a, d, q):
-    return Matrix([[cos(q), -sin(q), 0, a],
+    return Matrix([[             cos(q),             -sin(q),           0,               a],
                    [sin(q) * cos(alpha), cos(q) * cos(alpha), -sin(alpha), -sin(alpha) * d],
-                   [sin(q) * sin(alpha), cos(q) * sin(alpha), cos(alpha), cos(alpha) * d],
-                   [0, 0, 0, 1]])
+                   [sin(q) * sin(alpha), cos(q) * sin(alpha),  cos(alpha),  cos(alpha) * d],
+                   [                  0,                   0,           0,               1]])
+
+# Create individual transformation matrices
+#
+T0_1 = transform(alpha0, a0, d1, q1).subs(dh_dict)
+T1_2 = transform(alpha1, a1, d2, q2).subs(dh_dict)
+T2_3 = transform(alpha2, a2, d3, q3).subs(dh_dict)
+T3_4 = transform(alpha3, a3, d4, q4).subs(dh_dict)
+T4_5 = transform(alpha4, a4, d5, q5).subs(dh_dict)
+T5_6 = transform(alpha5, a5, d6, q6).subs(dh_dict)
+T6_EE = transform(alpha6, a6, d7, q7).subs(dh_dict)
+T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
+
 
 #Define Rotation Matrices
 #
@@ -62,17 +76,6 @@ def z_rotate(q):
     return Matrix([[cos(q), -sin(q), 0],
                    [sin(q), cos(q), 0],
                    [0, 0, 1]])
-
-# Create individual transformation matrices
-#
-T0_1 = transform(alpha0, a0, d1, q1).subs(dh_dict)
-T1_2 = transform(alpha1, a1, d2, q2).subs(dh_dict)
-T2_3 = transform(alpha2, a2, d3, q3).subs(dh_dict)
-T3_4 = transform(alpha3, a3, d4, q4).subs(dh_dict)
-T4_5 = transform(alpha4, a4, d5, q5).subs(dh_dict)
-T5_6 = transform(alpha5, a5, d6, q6).subs(dh_dict)
-T6_EE = transform(alpha6, a6, d7, q7).subs(dh_dict)
-T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
 
 # Rotation discrepancy correction factor for orientation difference between URDF and DH table
 R_COR = z_rotate(to_radians(180)) * y_rotate(to_radians(-90))
